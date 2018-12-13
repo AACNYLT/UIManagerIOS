767cec878e287e20d7e8ea40d00a16798a0390f3
Aroon Narayanan <aroon@gatech.edu>
2018-12-12 23:12:15 -0500

    Initial Commit

---
+++ NYLT UI Manager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NYLT UI Manager
+//
+//  Created by Aroon Narayanan on 12/12/18.
+//  Copyright © 2018 Atlanta Area Council NYLT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
